076f3c0cb7b00e15bd6026dc05a6bfbed0066d6b
Omar-Assem <161471312+Omar-Assem@users.noreply.github.com>
2025-04-12 23:58:22 +0200

    k

---
+++ src/Card/Card.jsx
@@ -0,0 +1,16 @@
+
+const Card = () => {
+  return (
+    <>
+      <div className="container  h-72">
+        <br />
+        <div className="card  ">
+            <h1 className="">hello</h1>
+            <h5>mora</h5>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Card
