22c60f29c303fb12f3ee12b943bddd3a8dc73722
Muhammad Hisham <muhammedheshamm2@gmail.com>
2025-04-21 19:42:47 +0200

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,10 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+server.use(cors());  
 server.use(middlewares);
 server.use(router);
 
