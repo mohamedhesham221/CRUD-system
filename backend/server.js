fd31a064505f7cb543e630470b28c429f4ca7b6f
Muhammad Hisham <muhammedheshamm2@gmail.com>
2025-04-21 23:26:52 +0200

    Update

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,24 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
 
+const PORT = 3000;
+
+// Enable CORS
+server.use(cors());
+
+
+// Use default middlewares and router
 server.use(middlewares);
-server.use('/api', router); // Route all requests to /api
+server.use(router);
 
-module.exports = (req, res) => {
-  server(req, res);
-};
+// Start server
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
+
+// Handle process termination
+process.on('SIGTERM', () => {
+  process.exit();
+});
